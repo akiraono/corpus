42cc8e25af9a7f94af15ce1df33ce2747d5e1eed
Akira Ono <akira.rainwalker@gmail.com>
2015-05-06 20:54:28 +0900

    add some files

---
+++ src/main/java/jp/akira/corpus/App.java
@@ -0,0 +1,13 @@
+package jp.akira.corpus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/jp/akira/corpus/Corpus.java
@@ -0,0 +1,5 @@
+public class Corpus {
+    public static void main(String[] args){
+	System.out.println("corpus");
+    }
+}
